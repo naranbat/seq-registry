f2fbeecc8a35ac7d06609d6964109c150aa08cca
naranbat <naranbat@jnaya.com>
2019-02-12 12:24:55 +0800

    updated

--- test/seq_registry.js
+++ test/seq_registry.js
@@ -11,13 +11,13 @@ contract("SEQ Registry test", accounts => {
         assert.equal(r1, false, "accounts[1] is in _Operators");
     });
 
-    it("Check registering and removing an operator", async () => {
+    it("Check adding and removing an operator", async () => {
         let instance = await SEQRegistry.deployed();
         
         let r0 = await instance.isOperator.call(accounts[2]);
 
         await instance.addOperator.call(accounts[2]);
-        let r2 = await instance.isOperator.call(accounts[2]);
+        let r1 = await instance.isOperator.call(accounts[2]);
 
         assert.equal(r0, false, "accounts[2] is already in _Operators");
         assert.equal(r1, true, "accounts[2] isn't in _Operators");
