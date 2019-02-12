309559deec37c94c974b96f98e016338b2a768c9
naranbat <naranbat@jnaya.com>
2019-02-12 11:41:17 +0800

    created simple contract for testing

---
+++ contracts/SimpleContract.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.5.2;
+
+import "./service/RegistryInterface.sol";
+
+contract SimpleContract {
+    address private _registryAddress;
+    
+    function changeRegistry(address registry) public returns (bool) {
+        _registryAddress = registry;
+        return true;
+    }
+
+    function check(address account) public view returns (uint256) {
+        RegistryInterface _registry = RegistryInterface(_registryAddress);
+        return _registry.check(account);
+    }
+}
